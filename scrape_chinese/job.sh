8877813c88c239ca0c4cc1f6ba5800c1917ddacd
Siyi Zhou <siyizhou@Siyis-MacBook-Pro.local>
2024-03-27 15:48:37 -0700

    added job files

---
+++ scrape_chinese/job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --partition=main
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=1:00:00
+
+module purge
+module load gcc/11.3.0
+module load python/3.9.12
+
+python collect_htmls.py
