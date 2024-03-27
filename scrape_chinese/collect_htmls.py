9c4cd802a8acd2930a02c908f50590247caf96a7
Siyi Zhou <siyizhou@Siyis-MacBook-Pro.local>
2024-03-27 15:57:12 -0700

    added debug iinfo

--- scrape_chinese/collect_htmls.py
+++ scrape_chinese/collect_htmls.py
@@ -6,10 +6,13 @@ import os
 def main():
     current = os.getcwd()
     directory_path =  current + '/scrape_chinese/all_links/'
+    f = open("log.txt", "a")
+    f.write("current_path: ")
+    f.write(directory_path)
+    f.write("\n")
     print(directory_path)
     all_files = os.listdir(directory_path)
     file_names = [item for item in all_files if os.path.isfile(os.path.join(directory_path, item))]
-    f = open("log.txt", "a")
     for n in file_names:
         print(n)
         dir_name = current+ "/htmls/" + n.split(".")[0]
